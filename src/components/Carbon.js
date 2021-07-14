75fbcbbbc141afef67c15866ce001e9be3e811f9
megan cho <meganlisacho@gmail.com>
2021-07-13 22:58:19 -0500

    fetched data successfully

--- src/components/Carbon.js
+++ src/components/Carbon.js
@@ -1,42 +1,31 @@
 import React, { Component } from 'react'
 
 class Carbon extends Component {
-
-    // constructor(){
-    //     super()
-    //     this.state = {
-    //         word: '', 
-    //         definition: '', 
-    //         pronunciation: ''
-    //     }
-    // }
-
-    // componentDidMount(){
-    //     this.fetchWord()
-    // }
-
-
-    fetchData = () => {
-        fetch(`https://raw.githubusercontent.com/owid/co2-data/master/owid-co2-data.json`)
-        .then(res => res.json())
-        .then(data => {
-            const info = data
-            this.setState({
-                carbon: info.co2[1], 
-                capita: info.co2_per_capita, 
-            })
-        })
+    constructor() {
+        super();
+        this.state = {
+            data: []
+        }
     }
 
+    componentDidMount() {
+        fetch('https://raw.githubusercontent.com/owid/co2-data/master/owid-co2-data.json')
+            .then( resp => resp.json())
+            .then((data)=> {
+                this.setState({
+                    data: data.url
+                })
+                console.log(data)
+            })
+    }
 
-    render(){
+    render() {
         return (
             <div>
-                {/* <h1>co2: {this.state.carbon}</h1>
-                <h3>co2 per capita: {this.state.capita}</h3> */}
-                <button onClick={this.fetchWord}>Try a different word!</button>
+                <h5>Co2 Emissions</h5>
+                <h5>Co2 Per Capita</h5>
             </div>
-        )
+        );
     }
 }
 
