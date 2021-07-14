de338e8b0cc136fc5f4f7705f3fffd4e44613978
megan cho <meganlisacho@gmail.com>
2021-07-13 22:31:08 -0500

    added text to home page

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Home extends Component {
+    render(){
+        return (
+            <div>
+                <h1>Our Planet in Data</h1>
+                <p>Data on CO2 and greenhouse gas emissions in our world today.</p>
+            </div>
+        )
+    }
+}
+
+export default Home
