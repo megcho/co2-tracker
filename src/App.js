6d4aab8879260f456814c3871be5247948d6b58a
megan cho <meganlisacho@gmail.com>
2021-07-23 14:25:07 -0500

    changed to dog api

--- src/App.js
+++ src/App.js
@@ -1,13 +1,52 @@
-import Home from './components/Home'
-import Carbon from './components/Carbon'
+import React, { Component } from 'react';
+import './App.css';
+import Select from './components/select/Select';
+import BreedImage from './components/breed-image/BreedImage';
 
-function App() {
-  return (
-    <div>
-    <Home />
-    <Carbon />
-    </div>
-  );
+class App extends Component {
+    state= {
+        breedsList: null,
+        selectedBreed: null,
+        error: false
+    }
+   componentDidMount() {
+       this.fetchAllBreeds();
+   }
+   fetchAllBreeds = async () => {
+       try { //try to get data
+        const response = await fetch('https://dog.ceo/api/breeds/list/all');
+        if (response.ok) { // ckeck if status code is 200
+            const data = await response.json();
+            this.setState({
+                breedsList: Object.keys(data.message)
+            })
+        } else {
+            this.setState({
+                error: true
+            })
+            alert('Sorry, can not display the data')
+        }
+        } catch (e) { //code will jump here if there is a network problem
+            this.setState ({
+                error: true
+            })
+            alert('Sorry, can not display the data')
+        }
+       }
+       selectHandler = (breed) => {
+           this.setState({
+               selectedBreed: breed
+           })
+       }
+    render( ) {
+        console.log(this.state.selectedBreed);
+        return (
+            <div className="App">
+                <Select breedsList={this.state.breedsList} onSelect={this.selectHandler} isError={this.state.error}/>
+                <BreedImage breed={this.state.selectedBreed}/>
+            </div>
+        );
+    }
 }
 
 export default App;
